51e0d114bea78188170c86b777a11fcbcb6bface
jnlar <jon357@protonmail.ch>
2020-12-07 14:18:05 +1100

    non-db solution for crud-- manipulation to <td> innerText

--- js/crud.js
+++ js/crud.js
@@ -1,43 +1,11 @@
-var arr;
-
 const util = {
-	uuid: function () {
-		/*jshint bitwise:false */
-		var i, random;
-		var uuid = '';
-
-		for (i = 0; i < 32; i++) {
-			random = Math.random() * 16 | 0;
-			if (i === 8 || i === 12 || i === 16 || i === 20) {
-				uuid += '-';
-			}
-			uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
-		}
-
-		return uuid;
-	},
-
-	store: function (namespace, data) {
-		if (arguments.length > 1) {
-			return localStorage.setItem(namespace, JSON.stringify(data));
-		} else {
-			var store = localStorage.getItem(namespace);
-			return (store && JSON.parse(store)) || [];
-		}
-	},
-
-	word: function(name, lastname) {
-		if (typeof(name) === 'string' && typeof(lastname) === 'string') { return true; }
-		return false;
-	},
+	clearInput: function() {
+		gid('crud-firstname').value = '';
+		gid('crud-lastname').value = '';
+	}
 }
 
-const crud = {
-	init: function() {
-		// TODO: do more stuff here
-		arr = util.store('crud');
-	},
-
+const app = {
 	selected: function() { 
 		let tr = qsa('tr');
 
@@ -48,8 +16,8 @@ const crud = {
 				}
 
 				tr[i].classList.add('selected');
-				// TODO: access corresponding object to 'selected' in crud object 
-				// in local storage, set name inputs to respective key pair values
+				gid('crud-firstname').value = tr[i].innerText.replace(/,.*/, '');
+				gid('crud-lastname').value = tr[i].innerText.replace(/\b(\w+)\W*/, '');
 			})
 		}
 	},
@@ -58,38 +26,25 @@ const crud = {
 		gid('crud-create').addEventListener('click', () => {
 			let firstName = gid('crud-firstname').value;
 			let lastName = gid('crud-lastname').value;
-
-			if (util.word(firstName, lastName) === true) {
-				arr.push({
-					id: util.uuid(),
-					firstName: firstName,
-					lastName: lastName
-				})
-			}
-
-			util.store('crud', arr);
-
 			let row = gid('crud-table').insertRow(0).insertCell(0);
 
-			row.innerHTML = `${firstName}, ${lastName}`
-			crud.selected();
+			row.innerHTML = `${firstName}, ${lastName}`;
+			app.selected();
+		})
+	})(),
+
+	update: (function() {
+		gid('crud-update').addEventListener('click', () => {
+			gcn('selected')[0].innerText = `${gid('crud-firstname').value}, ${gid('crud-lastname').value}`
+			console.log(gcn('selected'));
 		})
 	})(),
 
 	delete: (function() {
 		gid('crud-delete').addEventListener('click', () => {
 			if (gcn('selected')[0] === undefined) return;
-
 			gcn('selected')[0].remove();
-			// TODO: remove corresponing obj in crud local storage
+			util.clearInput();
 		})
 	})(),
-
-	// TODO: 
-	// 1. also update corresponding crud object in local storage
-	update: (function() {
-	// don't think we need to IIFE this
-	})(),
 }
-
-crud.init();

3853be5524d92b8d5044017d183a45a21014e225
jnlar <jon357@protonmail.ch>
2020-12-08 12:51:45 +1100

    crud filter implementation - doesn't exclusively filter by lastname yet

--- js/crud.js
+++ js/crud.js
@@ -1,12 +1,37 @@
 const util = {
 	clearInput: function() {
-		gid('crud-firstname').value = '';
-		gid('crud-lastname').value = '';
+		gid('crud-firstname').value = ''; gid('crud-lastname').value = '';
+	},
+
+	filter: function() {
+		// Credit - w3schools: https://www.w3schools.com/howto/howto_js_filter_lists.asp
+		let input, filter, td, a, txtValue;
+		input = gid('crud-filter');
+		filter = input.value.toUpperCase();
+		td = gtn('td');
+
+		for (let i = 0; i < td.length; i++) {
+			a = td[i];
+			txtValue = a.textContent || a.innerText;
+
+			if (txtValue.toUpperCase().indexOf(filter) > -1) {
+				td[i].style.display = "";
+			} else {
+				td[i].style.display = "none";
+			}
+		}
 	}
 }
 
 const app = {
-	selected: function() { 
+	init: function() {
+		this.crudCreate();
+		this.crudUpdate();
+		this.crudDelete();
+		this.crudFilter();
+	},
+
+	crudSelected: function() { 
 		let tr = qsa('tr');
 
 		for (let i = 0; i < tr.length; i++) {
@@ -22,29 +47,35 @@ const app = {
 		}
 	},
 
-	create: (function() {
+	crudCreate: function() {
 		gid('crud-create').addEventListener('click', () => {
 			let firstName = gid('crud-firstname').value;
 			let lastName = gid('crud-lastname').value;
 			let row = gid('crud-table').insertRow(0).insertCell(0);
 
 			row.innerHTML = `${firstName}, ${lastName}`;
-			app.selected();
+			app.crudSelected();
 		})
-	})(),
+	},
 
-	update: (function() {
+	crudUpdate: function() {
 		gid('crud-update').addEventListener('click', () => {
 			gcn('selected')[0].innerText = `${gid('crud-firstname').value}, ${gid('crud-lastname').value}`
 			console.log(gcn('selected'));
 		})
-	})(),
+	},
 
-	delete: (function() {
+	crudDelete: function() {
 		gid('crud-delete').addEventListener('click', () => {
 			if (gcn('selected')[0] === undefined) return;
 			gcn('selected')[0].remove();
 			util.clearInput();
 		})
-	})(),
+	},
+
+	crudFilter: function() {
+		gid('crud-filter').addEventListener('keyup', () => { util.filter(); })
+	},
 }
+
+app.init();

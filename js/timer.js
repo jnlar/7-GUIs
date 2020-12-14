3e1ce327691fd60ea06051fe1000adb6df128368
jnlar <jon357@protonmail.ch>
2020-12-14 19:10:50 +1100

    didn't like that the timer app was always running because setInterval() initiated on page load instead of click, created a start and stop button to resolve

--- js/timer.js
+++ js/timer.js
@@ -1,7 +1,12 @@
 const range = gid('time-range');
 const gauge = gid('time-gauge');
+var time;
 
 gauge.max = range.value;
-range.addEventListener('change', () => { gauge.max = range.value; });
-gid('time-reset').addEventListener('click', () => {gauge.value = 0})
-setInterval(function() { gauge.value += 0.1; gid('elapsed').innerHTML = `Elapsed: <em>${parseFloat(gauge.value).toFixed(1)}s</em>`; }, 100)
+
+range.addEventListener('change', () => { gauge.max = range.value });
+gid('time-reset').addEventListener('click', () => { gauge.value = 0 });
+gid('time-stop').addEventListener('click', () => { clearInterval(time) });
+gid('time-start').addEventListener('click', () => { clearInterval(time);
+	time = setInterval(function() { gauge.value += 0.1; gid('elapsed').innerHTML = `Elapsed: <em>${parseFloat(gauge.value).toFixed(1)}s</em>`; }, 100);
+})
